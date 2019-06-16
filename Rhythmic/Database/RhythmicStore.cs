916981a184a87796dc970922d1adeefd51ab3a9d
Alten <altenyt@gmail.com>
2019-06-16 18:12:16 -0400

    Add yellow & new store

---
+++ Rhythmic/Database/RhythmicStore.cs
@@ -0,0 +1,13 @@
+﻿using osu.Framework.Bindables;
+using osuTK.Graphics;
+
+namespace Rhythmic.Database
+{
+    /// <summary>Includes stuff such as Primary Colors, Beatmaps, Scores, etc.</summary>
+    public class RhythmicStore
+    {
+        public Bindable<Color4> PrimaryColour = new Bindable<Color4>();
+
+        public Bindable<Color4> SecondaryColour = new Bindable<Color4>();
+    }
+}
