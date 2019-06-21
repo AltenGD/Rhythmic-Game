701f7b2efb795361bf0d8ce4dce5f0cb08b4e56e
Alten <altenyt@gmail.com>
2019-06-20 22:26:19 -0400

    Start SongSelect from scratch

--- Rhythmic/Screens/Select/SongSelect.cs
+++ Rhythmic/Screens/Select/SongSelect.cs
@@ -1,7 +1,5 @@
 ﻿using osu.Framework.Screens;
 using Rhythmic.Screens.Backgrounds;
-using osu.Framework.Graphics;
-using Rhythmic.Screens.Select.Components;
 using osu.Framework.Allocation;
 using Rhythmic.Beatmap;
 
@@ -13,24 +11,12 @@ namespace Rhythmic.Screens.Select
 
         protected override BackgroundScreen CreateBackground() => new BackgroundScreenBeatmap();
 
-        public override bool DisableBeatmapOnEnter => false;
-
         [Resolved]
         private BeatmapCollection collection { get; set; }
 
         public SongSelect()
         {
-            AddRangeInternal(new Drawable[]
-            {
-                new BeatmapLevelListing
-                {
-                    RelativeSizeAxes = Axes.Both,
-                    selectedBeatmap = delegate
-                    {
-                        this.Push(new InfoScreen());
-                    }
-                },
-            });
+
         }
 
         public override void OnResuming(IScreen last)
